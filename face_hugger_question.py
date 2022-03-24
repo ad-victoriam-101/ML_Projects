ab05e939ec48e8ca606bf8cc805dc07e8a7b3660
ad_victoriam_101 <keeper24@gmail.com>
2022-03-24 11:19:59 -0700

    test

--- face_hugger_question.py
+++ face_hugger_question.py
@@ -122,3 +122,24 @@ model = TFAutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
 import tensorflow as tf
 
 from tensorflow import keras
+
+optimizer = keras.optimizers.Adam(learning_rate=5e-5)
+
+keras.mixed_precision.set_global_policy("mixed_float16")
+
+model.compile(optimizer=optimizer)
+
+context = """Keras is an API designed for human beings, not machines. Keras follows best
+practices for reducing cognitive load: it offers consistent & simple APIs, it minimizes
+the number of user actions required for common use cases, and it provides clear &
+actionable error messages. It also has extensive documentation and developer guides. """
+question = "What is Keras?"
+
+inputs = tokenizer([context], [question], return_tensors="np")
+outputs = model(inputs)
+start_position = tf.argmax(outputs.start_logits, axis=1)
+end_position = tf.argmax(outputs.end_logits, axis=1)
+print(int(start_position), int(end_position[0]))
+
+answer = inputs["input_ids"][0, int(start_position) : int(end_position) + 1]
+print(tokenizer.decode(answer))
